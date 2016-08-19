2dd7dc9676db60c26bf0a54ca1de0bca966fce8a
Joshua McKinney <joshka@users.noreply.github.com>
2016-08-19 17:27:24 -0500

    Add brew-cash-zsh-completion

---
+++ brew-cask-zsh-completion.rb
@@ -0,0 +1,18 @@
+class BrewCaskZshCompletion < Formula
+  desc "Zsh completion for brew-cask"
+  homepage "https://github.com/joshka/brew-cask-zsh-completions"
+  url "https://github.com/joshka/brew-cask-zsh-completions/archive/v1.0.tar.gz"
+  sha256 "ec274779e14af69a76a3b884f8fcf0b127692e6cb36f8799f76635da068243e0"
+  head "https://github.com/joshka/brew-cask-zsh-completions.git"
+
+  bottle :unneeded
+
+  def install
+    zsh_completion.install "_brew_cask"
+  end
+
+  test do
+    assert_match "_brew_cask",
+      shell_output("zsh -c 'fpath+=#{zsh_completion}; autoload -U compinit && compinit; whence _brew_cask'")
+  end
+end
